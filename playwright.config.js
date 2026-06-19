53f97d60df7c2b7862bbcf4ac63762471fa642b2
Lera Chuiko <lera.chuiko@gmail.com>
2026-06-19 21:35:44 +0200

    Update config with userAgent and timeouts

--- playwright.config.js
+++ playwright.config.js
@@ -10,6 +10,9 @@ export default defineConfig({
     trace: 'on-first-retry',
     video: 'on',      // Прямое включение видео
     screenshot: 'on', // Прямое включение скриншотов
+    userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36',
+    navigationTimeout: 60000,
+    actionTimeout: 60000,
   },
 
   projects: [
